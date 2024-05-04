6a562c2ff00b9f091792e60e89e73bd46711cf98
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2024-05-05 01:29:16 +0900

    updated to 1.20.6

--- src/client/java/io/github/hashibutogarasu/mla/ModMenuIntegration.java
+++ src/client/java/io/github/hashibutogarasu/mla/ModMenuIntegration.java
@@ -1,13 +1,12 @@
 package io.github.hashibutogarasu.mla;
 
-import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-import com.terraformersmc.modmenu.api.ModMenuApi;
-import io.github.hashibutogarasu.mla.config.ModConfig;
-import me.shedaniel.autoconfig.AutoConfig;
+//public class ModMenuIntegration implements ModMenuApi {
+//    @Override
+//    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+//        return parent -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
+//    }
+//}
 
-public class ModMenuIntegration implements ModMenuApi {
-    @Override
-    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
-    }
+public class ModMenuIntegration{
+
 }
